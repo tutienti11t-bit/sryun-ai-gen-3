6c761379f2778b973b86796bfcf217c3a805a953
tutienti11t-bit <tutienti11t@gmail.com>
2025-12-23 22:03:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
-    server: { port: 3000, host: "0.0.0.0" },
     plugins: [react()],
-    resolve: { alias: { "@": path.resolve(__dirname, ".") } },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+    },
   };
 });
